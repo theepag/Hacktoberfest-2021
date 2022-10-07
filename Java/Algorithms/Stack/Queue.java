f7115190a45b3b2afeda67a9019b040e37aa411e
Vansh <rs1030660@gmail.com>
2022-10-07 08:47:24 +0530

    # New-Added

---
+++ Java/Algorithms/Stack/Queue.java
@@ -0,0 +1,35 @@
+// Queue using array
+
+import java.util.*;
+
+public class Queue{
+    static class Queue {
+        static int arr[];
+        static int size;
+        static int rear ;
+
+        Queue (int size) {
+            this.size = size ;
+            size = new int[size];
+            raer = -1 ;
+
+        }
+         public static boolean isEmpty() {
+            return rear == -1 ;
+         } 
+    }
+
+    public static boolean isFull(){
+        return rear == size-1;
+
+    }
+
+    public static void add(int data){
+        if(isFull()) {
+            System.our=t.println("Overflow");
+            return;
+        }
+
+        arr[++rear] = data;
+    }
+}
